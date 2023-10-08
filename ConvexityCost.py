d9176475d678eec11f6fc46c6d1871e6334ad323
AnishP15 <39468239+AnishP15@users.noreply.github.com>
2023-10-07 21:14:54 -0400

    Create ConvexityCost.py

---
+++ ConvexityCost.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import jarque_bera
+import matplotlib.pyplot as plt
+from coinpaprika import client as Coinpaprika
+from statsmodels.tsa.arima.model import ARIMA
+
+
+client = Coinpaprika.Client()
+
+
+raw = client.historical(
+"eth-ethereum", start="2023-01-11T00:00:00Z", limit=5000, interval='1m')
+df = pd.DataFrame(raw)
+
+
+df = df[['timestamp', 'price']]
+df.columns = ['date', 'price']
+
+
+
+
+# Calculate log returns
+df['LogReturns'] = np.log(df['price'] / df['price'].shift(periods=1))
+y = df['LogReturns']
+
+
+# Compute quadratic variation
+QV = []
+for i in range(10, len(y), 10):
+QV.append(np.sum(np.diff(y[i-10:i])**2))
+QV = pd.Series(QV)
+
+
+
+
+# Standardize
+z = (QV - QV.mean()) / QV.std()
+
+
+model = ARIMA(z, order=(2,1,2))
+results = model.fit()
+
+
+print(results.summary())
+
+
+_, ax = plt.subplots()
+
+
+ax.plot(df['price'], label='Exchange Rate')
+
+
+ax.set_xlabel('Date')
+ax.set_ylabel('Exchange Rate (ETH/USD)')
+ax.legend()
+
+
+_, ax2 = plt.subplots()
+
+
+ax2.plot(z, label='Quadratic Variation')
+
+
+ax2.set_xlabel('Periods')
+ax2.set_ylabel('Quadratic Variation (ETH/USD)')
+ax2.legend()
+
+
+#plt.show()
+
