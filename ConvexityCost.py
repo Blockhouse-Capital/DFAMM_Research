d07f744d70906ec322a9dbc2323a6eae00c0fa36
AnishP15 <39468239+AnishP15@users.noreply.github.com>
2023-10-08 13:33:11 -0400

    fix: Update requirements.txt

--- ConvexityCost.py
+++ ConvexityCost.py
@@ -8,7 +8,6 @@ from statsmodels.tsa.arima.model import ARIMA
 
 client = Coinpaprika.Client()
 
-
 raw = client.historical(
 "eth-ethereum", start="2023-01-11T00:00:00Z", limit=5000, interval='1m')
 df = pd.DataFrame(raw)
@@ -17,13 +16,9 @@ df = pd.DataFrame(raw)
 df = df[['timestamp', 'price']]
 df.columns = ['date', 'price']
 
-
-
-
 # Calculate log returns
 df['LogReturns'] = np.log(df['price'] / df['price'].shift(periods=1))
 y = df['LogReturns']
-
 
 # Compute quadratic variation
 QV = []
@@ -34,7 +29,6 @@ QV = pd.Series(QV)
 # Standardize
 z = (QV - QV.mean()) / QV.std()
 
-
 model = ARIMA(z, order=(2,1,2))
 results = model.fit()
 
@@ -42,20 +36,16 @@ results = model.fit()
 
 _, ax = plt.subplots()
 
-
 ax.plot(df['price'], label='Exchange Rate')
 
 
 ax.set_xlabel('Date')
-ax.set_ylabel('Exchange Rate (ETH/USD)')
+ax.set_ylabel('Exchange Rate (ETH/UNUSD)')
 ax.legend()
-
 
 _, ax2 = plt.subplots()
 
-
 ax2.plot(z, label='Quadratic Variation')
-
 
 ax2.set_xlabel('Periods')
 ax2.set_ylabel('Quadratic Variation (ETH/USD)')
---
+++ GenerateUsdcData.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import pandas as pd
+
+token_usdc_id = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+
+url = 'https://api.thegraph.com/subgraphs/name/ianlapham/v3-minimal'
+frames = []
+query = """query {
+tokenDayDatas (where : {token: "%s"}, orderBy: date, orderDirection: desc, skip:%d)
+{
+date
+volumeUSD
+totalValueLockedUSD
+priceUSD
+feesUSD
+open
+high
+low
+close
+}
+}"""
+for i in range(0,500,10):
+    tmp_query = query%(token_usdc_id,i)
+    r = requests.post(url, json={'query': tmp_query})
+    json_data = json.loads(r.text)
+    try:
+        df_data = json_data['data']["tokenDayDatas"]
+        df = pd.DataFrame(df_data)
+        frames.append(df)
+    except KeyError:
+        break
+
+result = pd.concat(frames)
+result['date'] = pd.to_datetime(result['date'], unit='s')
+result[["volumeUSD","totalValueLockedUSD","feesUSD","priceUSD","open","high","low","close"]] = result[["volumeUSD","totalValueLockedUSD","feesUSD","priceUSD","open","high","low","close"]].apply(pd.to_numeric)
+result['token_id'] = token_usdc_id
+result.to_csv("USDC_Data.csv")
+
+print(result.head())
+
+
